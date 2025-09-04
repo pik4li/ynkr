8757d64b4d86f2109f2c39545ddd80ebbbdec977
Alexander Pieck <pika@team-pieck.de>
2025-09-04 14:57:49 +0200

    added setup script for initializing the directories for direct use

--- setup.sh
+++ setup.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+command-exists() {
+  command -v "$@" >/dev/null 2>&1
+}
+
+build-ynkr() {
+  if command-exists docker; then
+    docker build -t ynkr:latest . || {
+      echo "You do not have docker installed on your system, or you might not be in the docker group."
+      echo "Cannot continue build process!"
+      exit 1
+    }
+  else
+    echo "You do not have docker installed on your system"
+    echo "Please install docker, and rerun this script"
+    exit 1
+  fi
+}
+
 check-dirs() {
   local dirs=(
     "archives"
@@ -8,9 +26,9 @@ check-dirs() {
     "music"
   )
 
-  for d in "${dirs[@]}"; do
-    if [[ ! -e "${d}" ]]; then
-      mkdir -p "${d}"
+  for dir in "${dirs[@]}"; do
+    if [[ ! -e "${dir}" ]]; then
+      mkdir -p "${dir}"
     fi
   done
 }
@@ -19,5 +37,10 @@ check-dirs() {
 check-dirs
 [[ ! -f ./db/music_imports.db ]] && touch ./db/music_imports.db # create database file
 
+# build ynkr:latest or exit, if
+build-ynkr
+
 echo "Setup is done, created ${dirs[*]} and ./db/music_imports.db"
-echo "You can now run ynkr with 'docker compose up -d'"
+echo "Also the ynkr:latest container was build."
+echo "Keep in mind, that you have to setup your playlists file, and your .env file"
+echo "You can then run ynkr with 'docker compose up -d'"
