251fd781243cf63c976d5a362423ff71687c1ef5
pika <git@team-pieck.de>
2026-01-22 23:33:26 +0100

    pushing testscripts

--- test.sh
+++ test.sh
@@ -4,3 +4,5 @@ cd "${0%/*}" >/dev/null 2>&1 || : # cd's into the right dir for sourcing..
 
 . lib/logging.sh
 . lib/parser.sh
+
+sanitize-metadata & # sub process for managing sanitization.. Will get addet in the future.
