6ac575ea97e8a78dfd67554f09cc768e0c45b6fe
pika <git@team-pieck.de>
2026-01-24 17:26:36 +0100

    wip

--- lib/ynkr.sh
+++ lib/ynkr.sh
@@ -90,7 +90,7 @@ ynkr:song() {
     "--concurrent-fragments=3"
     "--retries=5"
     "--download-archive=$YT_ARCHIVE.yt"
-    "--print='%(colors.green)s▶%(colors.reset)s %(title)s\n%(colors.blue)s👤 %(uploader)s%(colors.reset)s | %(colors.magenta)s⏱ %(duration_string)s%(colors.reset)s | %(colors.cyan)s📺 %(resolution)s%(colors.reset)s | %(colors.yellow)s🎵 %(format_note)s%(colors.reset)s\n%(colors.gray)s%(webpage_url)s%(colors.reset)s\n'"
+    "--print=%(colors.green)s[▶]%(colors.reset)s %(title)s\n%(colors.blue)s[]%(colors.reset)s %(uploader)s  %(colors.magenta)s[]%(colors.reset)s %(duration_string)s  %(colors.cyan)s[󰍹]%(colors.reset)s %(resolution)s  %(colors.yellow)s[]%(colors.reset)s %(format_note)s\n%(colors.gray)s[]%(colors.reset)s %(webpage_url)s\n"
     "$url"
   )
 
